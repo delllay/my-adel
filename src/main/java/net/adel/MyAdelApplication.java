4e7d6292a242dd3203edc2c53fc21213321da4e3
j <delllay@gmail.com>
2021-02-03 21:16:23 +0900

    initial commit

---
+++ src/main/java/net/adel/MyAdelApplication.java
@@ -0,0 +1,13 @@
+package net.adel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAdelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAdelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/adel/MyAdelApplicationTests.java
@@ -0,0 +1,13 @@
+package net.adel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAdelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
